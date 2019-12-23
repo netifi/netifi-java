6d5556a62d31daca910dd61c458ce1d0b730b9cb
Alan Scherger <alan@netifi.com>
2019-12-23 10:31:27 -1000

    ci: get building on os x

--- netifi-spring-core/src/test/java/com/netifi/spring/core/TestIdlServiceServer.java
+++ netifi-spring-core/src/test/java/com/netifi/spring/core/TestIdlServiceServer.java
@@ -15,12 +15,11 @@
  */
 package com.netifi.spring.core;
 
-import java.util.Map;
-
 import io.rsocket.Payload;
 import io.rsocket.ipc.util.IPCChannelFunction;
 import io.rsocket.ipc.util.IPCFunction;
 import io.rsocket.rpc.AbstractRSocketService;
+import java.util.Map;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -39,10 +38,9 @@ public class TestIdlServiceServer extends AbstractRSocketService {
   }
 
   @Override
-  public void selfRegister(Map<String, IPCFunction<Mono<Void>>> fireAndForgetRegistry,
-          Map<String, IPCFunction<Mono<Payload>>> requestResponseRegistry,
-          Map<String, IPCFunction<Flux<Payload>>> requestStreamRegistry,
-          Map<String, IPCChannelFunction> requestChannelRegistry) {
-
-  }
+  public void selfRegister(
+      Map<String, IPCFunction<Mono<Void>>> fireAndForgetRegistry,
+      Map<String, IPCFunction<Mono<Payload>>> requestResponseRegistry,
+      Map<String, IPCFunction<Flux<Payload>>> requestStreamRegistry,
+      Map<String, IPCChannelFunction> requestChannelRegistry) {}
 }
